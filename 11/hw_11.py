1c6e275942501d77851768d77904aaf3b0e6e76c
Yurii Pokotylo <yzon81@gmail.com>
2022-11-10 13:52:13 +0200

    lesson12

---
+++ 11/hw_11.py
@@ -0,0 +1,113 @@
+# Задача №1
+# 1. Доопрацюйте класс Point так, щоб в атрибути x та y обʼєктів цього класу можна було записати тільки обʼєкти класу
+# int або float
+# 2. Створіть класс Triangle (трикутник), який задається трьома точками (обʼєкти классу Point). Реалізуйте перевірку
+# даних, аналогічно до класу Line. Визначет атрибут, що містить площу трикутника (за допомогою property).
+# Для обчислень можна використати формулу Герона (https://en.wikipedia.org/wiki/Heron%27s_formula)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if isinstance(value, (float, int)):
+            self._x = value
+        else:
+            raise TypeError('x it should be float or int')
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        if isinstance(value, (float, int)):
+            self._y = value
+        else:
+            raise TypeError('y it should be float or int')
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    @property
+    def p1(self):
+        return self._p1
+
+    @p1.setter
+    def p1(self, value):
+        if isinstance(value, Point):
+            self._p1 = value
+        else:
+            raise TypeError('p1 it should be Point')
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, value):
+        if isinstance(value, Point):
+            self._p2 = value
+        else:
+            raise TypeError('p2 it should be Point')
+
+    @property
+    def length(self):
+        return ((self.p1.x - self.p2.x) ** 2 + (self.p1.y - self.p2.y) ** 2) ** 0.5
+
+
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    @property
+    def p1(self):
+        return self._p1
+
+    @p1.setter
+    def p1(self, value):
+        if isinstance(value, Point):
+            self._p1 = value
+        else:
+            raise TypeError('p1 it should be  Point')
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, value):
+        if isinstance(value, Point):
+            self._p2 = value
+        else:
+            raise TypeError('p2 it should be Point')
+
+    @property
+    def p3(self):
+        return self._p3
+
+    @p3.setter
+    def p3(self, value):
+        if isinstance(value, Point):
+            self._p3 = value
+        else:
+            raise TypeError('p3 it should be Point')
+
+    @property
+    def area(self):
+        a = Line(self.p1, self.p2).length
+        b = Line(self.p2, self.p3).length
+        c = Line(self.p3, self.p1).length
+        p = (a + b + c) / 2
+        return (p * (p - a) * (p - b) * (p - c)) ** 0.5
