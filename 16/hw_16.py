21e1bfd033cae9b8951a581ed4b69891ae20f953
Yurii Pokotylo <yzon81@gmail.com>
2022-11-23 10:22:34 +0200

    lesson16

---
+++ 16/hw_16.py
@@ -0,0 +1,20 @@
+import requests
+
+bank_url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+
+try:
+    res = requests.get(bank_url)
+except:
+    print('Some exception')
+else:
+    if 300 > res.status_code >= 200:
+        try:
+            resp_json = res.json()
+        except:
+            print('Json exception')
+        else:
+            print(type(resp_json))
+            print(resp_json)
+
+        with open('hw_16-txt.txt', 'wb') as file:
+            file.write(res.content)
